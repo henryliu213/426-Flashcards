08c7f395e0f62d56925dd51e3ed4b0c905f99cd3
HenryLiu213 <hliu1@unc.edu>
2024-12-25 13:29:25 -0500

    test

--- Backend/db.py
+++ Backend/db.py
@@ -7,10 +7,10 @@ cursor = db.cursor()
 
 
 
-cursor.execute("Drop database flashcards")
-# cursor.execute("Show databases")
-# clist = [i for i in cursor.fetchall()] 
-# print(clist)
+# cursor.execute("Drop database flashcards")
+cursor.execute("Show databases")
+clist = [i for i in cursor.fetchall()] 
+print(clist)
 
 cursor.execute("CREATE DATABASE if not exists flashcards ")
 cursor.execute("Show databases")
